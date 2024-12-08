ad4f4f09d923c7e04366cab11e16f7a5b3127aae
hpe-palermo <hpalermoemerick@gmail.com>
2024-12-08 20:03:11 -0300

    feat: instalação do docker ao inicializar a instância

--- Desafio02/user_data.sh
+++ Desafio02/user_data.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 # Atualiza o sistema e instala dependências
-sudo apt-get update -y
-sudo apt-get upgrade -y
-sudo apt-get install -y docker.io
-
-sudo apt-get install -y unzip curl
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-
+sudo yum update -y
+sudo yum upgrade -y
+sudo yum install -y docker
 
 # Inicia o Docker e configura para iniciar com o sistema
 sudo systemctl start docker
 sudo systemctl enable docker
 
 # Adiciona o usuário atual ao grupo docker
-sudo usermod -aG docker ubuntu
+sudo usermod -aG docker ec2-user
 
 # Instala o Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
