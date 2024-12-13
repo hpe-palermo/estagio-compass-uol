a9de16271234dda905f5b7f5ebfaf2cc2d80e665
hpe-palermo <hpalermoemerick@gmail.com>
2024-12-13 09:52:16 -0300

    feat: conexao das instancias com o efs

--- Desafio02/user_data.sh
+++ Desafio02/user_data.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
+
+# Atualizar pacotes
 sudo yum update -y
 sudo yum upgrade -y
+
+# Instalar Docker
 sudo yum install -y docker
 
+# Iniciar e habilitar o Docker
 sudo systemctl start docker
 sudo systemctl enable docker
 
+# Adicionar o usuário atual ao grupo Docker
 sudo usermod -aG docker ec2-user
 
+# Aplicar alterações de grupo sem reiniciar
+newgrp docker
+
+# Instalar Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Reinicia para aplicar permissões(encontrar outra maneira)
-sudo reboot
+echo "Instalação concluída. Você pode usar o Docker sem reiniciar o sistema."
 
-# iniciar o nginx
+# Criar o arquivo docker-compose.yml
 cat <<EOF > docker-compose.yml
 version: '3.9'
 
@@ -24,7 +33,7 @@ services:
     image: nginx
     restart: always
     ports:
-      - 8080:80
+      - 80:80
     volumes:
       - nginx-v:/usr/share/nginx/html  # Caminho correto para o diretório padrão do nginx
 
@@ -32,7 +41,31 @@ volumes:
   nginx-v:
 EOF
 
-dokcer-compose up -d
+cat <<EOF > docker-compose.yml
+version: '3.9'
+
+services:
+  nginx:
+    image: nginx
+    restart: always
+    ports:
+      - 80:80
+    volumes:
+      - /mnt/dados:/mnt/dados
+EOF
+
+docker run -d --name nginx -v /nginx-efs:/usr/share/nginx/html -p 80:80 nginx
+
+
+# Iniciar o NGINX com Docker Compose
+docker-compose up -d
+
+# Obter o endereço IP da máquina
+IP=$(hostname -I | awk '{print $1}')
+
+# Exibir a mensagem final com o IP dinâmico
+echo "NGINX iniciado. Acesse o servidor em http://$IP:80"
+
 
 # iniciar o wordpress
 cat <<EOF > docker-compose.yml
@@ -71,6 +104,18 @@ services:
 volumes:
   nginx:
 EOF
+
+# Instalar nfs
+sudo yum -y install nfs-utils
+sudo systemctl enable nfs-client.target
+sudo systemctl start nfs-client.target
+
+# Montagem manual
+# sudo mount -t nfs <servidor_nfs>:<diretório_compartilhado> /<ponto_de_montagem>
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-036b70ebd35018960.efs.us-east-1.amazonaws.com:/ /nginx-efs
+
+# Montagem automática (arquivo /etc/fstab)
+# <servidor_nfs>:<diretório_compartilhado>  /<ponto_de_montagem>  nfs  defaults  0  0
 
 # Instalar o Cliente MySQL no Amazon Linux
 sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm
