bad2661685b37ec4766e03a8fa43a8a885794fe9
hpe-palermo <hpalermoemerick@gmail.com>
2024-12-16 16:54:13 -0300

    fix: consertando automação

--- Desafio02/user_data.sh
+++ Desafio02/user_data.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Variáveis
+export ENDERECO_EFS="fs-076301ce247ea6dcc.efs.us-east-1.amazonaws.com"
+export WORDPRESS_DB_HOST="my-database.c1ue0wu6y89k.us-east-1.rds.amazonaws.com"
+export WORDPRESS_DB_USER="admin"
+export WORDPRESS_DB_PASSWORD="nduhr02-02ffk4"
+export WORDPRESS_DB_NAME="dp_wp"
+
 # Atualizar pacotes
 sudo yum update -y
 sudo yum upgrade -y
@@ -11,63 +18,29 @@ sudo yum install -y docker
 sudo systemctl start docker
 sudo systemctl enable docker
 
-# Adicionar o usuário atual ao grupo Docker
-sudo usermod -aG docker ec2-user
-
-# Aplicar alterações de grupo sem reiniciar
-newgrp docker
+# Mudar para o usuário 'root'
+sudo su
 
 # Instalar Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
-echo "Instalação concluída. Você pode usar o Docker sem reiniciar o sistema."
+# Instalar nfs
+sudo yum -y install nfs-utils
+sudo systemctl enable nfs-client.target
+sudo systemctl start nfs-client.target
 
-# Criar o arquivo docker-compose.yml
-cat <<EOF > docker-compose.yml
-version: '3.9'
+# Montagem do sistema de arquivos
+sudo mkdir -p /mnt/dados
+echo "$ENDERECO_EFS:/ /mnt/dados nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 0 0" | sudo tee -a /etc/fstab
 
-services:
+# Limpar cache NFS
+echo 3 > /proc/sys/vm/drop_caches
 
-  nginx:
-    image: nginx
-    restart: always
-    ports:
-      - 80:80
-    volumes:
-      - nginx-v:/usr/share/nginx/html  # Caminho correto para o diretório padrão do nginx
+# Reiniciar o sistema de arquivos
+sudo systemctl restart sudo systemctl restart nfs-utils
 
-volumes:
-  nginx-v:
-EOF
-
-cat <<EOF > docker-compose.yml
-version: '3.9'
-
-services:
-  nginx:
-    image: nginx
-    restart: always
-    ports:
-      - 80:80
-    volumes:
-      - /mnt/dados:/mnt/dados
-EOF
-
-docker run -d --name nginx -v /nginx-efs:/usr/share/nginx/html -p 80:80 nginx
-
-
-# Iniciar o NGINX com Docker Compose
-docker-compose up -d
-
-# Obter o endereço IP da máquina
-IP=$(hostname -I | awk '{print $1}')
-
-# Exibir a mensagem final com o IP dinâmico
-echo "NGINX iniciado. Acesse o servidor em http://$IP:80"
-
-
-# iniciar o wordpress
+# Iniciar o WordPress
 cat <<EOF > docker-compose.yml
 version: '3.9'
 
@@ -79,48 +52,80 @@ services:
     ports:
       - 8080:80
     environment:
-      WORDPRESS_DB_HOST: db-wp.c1ue0wu6y89k.us-east-1.rds.amazonaws.com
-      WORDPRESS_DB_USER: admin
-      WORDPRESS_DB_PASSWORD: nduhr02-02ffk4
-      WORDPRESS_DB_NAME: db_wp
+      WORDPRESS_DB_HOST: $WORDPRESS_DB_HOST
+      WORDPRESS_DB_USER: $WORDPRESS_DB_USER
+      WORDPRESS_DB_PASSWORD: $WORDPRESS_DB_PASSWORD
+      WORDPRESS_DB_NAME: $WORDPRESS_DB_NAME
     volumes:
       - wordpress:/var/www/html
 
 volumes:
   wordpress:
-
-version: '3.9'
-
-services:
-
-  nginx:
-    image: nginx
-    restart: always
-    ports:
-      - 8080:80
-    volumes:
-      - nginx-v:/usr/share/nginx/html
-
-volumes:
-  nginx:
+    driver_opts:
+      type: nfs
+      o: addr=$ENDERECO_EFS,rw
+      device: ":/mnt/dados"
 EOF
 
-# Instalar nfs
-sudo yum -y install nfs-utils
-sudo systemctl enable nfs-client.target
-sudo systemctl start nfs-client.target
+# Ou com 'docker run'
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-076301ce247ea6dcc.efs.us-east-1.amazonaws.com:/ /mnt/dados
+docker rm -f wordpress
+docker run -d \
+  --name wordpress \
+  -p 80:80 \
+  -e WORDPRESS_DB_HOST=$WORDPRESS_DB_HOST \
+  -e WORDPRESS_DB_USER=$WORDPRESS_DB_USER \
+  -e WORDPRESS_DB_PASSWORD=$WORDPRESS_DB_PASSWORD \
+  -e WORDPRESS_DB_NAME=$WORDPRESS_DB_NAME \
+  -v /mnt/dados:/var/www/html \
+  --restart always \
+  wordpress
 
-# Montagem manual
-# sudo mount -t nfs <servidor_nfs>:<diretório_compartilhado> /<ponto_de_montagem>
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport fs-036b70ebd35018960.efs.us-east-1.amazonaws.com:/ /nginx-efs
 
-# Montagem automática (arquivo /etc/fstab)
-# <servidor_nfs>:<diretório_compartilhado>  /<ponto_de_montagem>  nfs  defaults  0  0
+# ------------------------------------------------------------------------------------------------------------
+
+# # Criar o arquivo docker-compose.yml
+# cat <<EOF > docker-compose.yml
+# version: '3.9'
+
+# services:
+
+#   nginx:
+#     image: nginx
+#     restart: always
+#     ports:
+#       - 8080:80
+#     volumes:
+#       - nginx-v:/usr/share/nginx/html  # Caminho correto para o diretório padrão do nginx
+
+# volumes:
+#   nginx-v:
+# EOF
+
+# Criar o arquivo docker-compose.yml
+# cat <<EOF > docker-compose.yml
+# version: '3.9'
+
+# services:
+#   nginx:
+#     image: nginx
+#     restart: always
+#     ports:
+#       - 8080:80
+#     volumes:
+#       - /mnt/dados:/mnt/dados
+# EOF
+
+# # Iniciar conteiner docker Nginx
+# docker run -d --name nginx -v /nginx-efs:/usr/share/nginx/html -p 80:80 nginx
+
+# # Iniciar o Docker Compose
+# docker-compose up -d
+
+# ------------------------------------------------------------------------------------------------------------
 
 # Instalar o Cliente MySQL no Amazon Linux
 sudo wget https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm
 sudo yum install mysql80-community-release-el9-1.noarch.rpm -y 
 sudo yum install mysql-community-server -y
-sudo rm -f /etc/pki/rpm-gpg/RPM-GPG-KEY-mysql
-sudo rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2023
 sudo systemctl start mysqld
---
+++ teste.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+export NOME="Hick" USERNAME="hk_hick" IDADE=19
+echo "Olá, $NOME($USERNAME)"
